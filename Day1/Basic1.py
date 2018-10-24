3bfc86216c51ce988bc8eea66119ee0928445191
jsh <jshjsh04@gmail.com>
2018-10-24 17:32:29 +0900

    Updatd Day1

--- Day1/Basic1.py
+++ Day1/Basic1.py
@@ -1,3 +1,4 @@
+### 표준 출력
 # 변수에 문자담기
 name = '홍길동'
 greeting = '안녕'
@@ -29,3 +30,36 @@ print('가격은 ' + str(coffee1_val + coffee2_val + coffee3_val) + '원 입니�
 coffee_val = coffee1_val + coffee2_val + coffee3_val
 print('손님, \n%s, %s, %s를 주문하셨습니다.' % (coffee1_name, coffee2_name, coffee3_name))
 print('가격은 %d원 입니다.' % coffee_val)
+
+### 표준 입력
+
+# Case 1
+name = input('당신의 이름은 무엇입니까? ')
+
+print(name + '님, 반갑습니다.')
+
+# Case 2
+order = input('OO카페입니다. \n무엇을 주문하시겠습니까? ')
+count = input('몇 잔을 드릴까요? ')
+
+print('%s %s잔을 주문하셨습니다. \n잠시만 기다려주세요~^^' % (order, count))
+
+# Case 3
+price = 4500
+cost  = price * int(count) # count 데이터 타입은 문자이므로 int형으로 바꿔줘야함
+
+print('%s %s잔을 주문하셨습니다. \n결재하실 금액은 %s원입니다~^^' % (order, count, cost))
+
+
+
+### 주석문
+# 이 줄은 라인 코멘트입니다
+print ("Hello World!")
+print ("Hello World!")    # 이것도 라인 코멘트입니다
+print ("Hello World!")    # 이것도 라인 코멘트입니다
+
+"""
+블럭주석, 
+즉 멀티라인의 주석문은 따옴표(''' ''') 3개
+"""
+# 한줄주석문은 샵(#)기호
---
+++ Day1/Turtle.py
@@ -0,0 +1,65 @@
+# 거북이 소개
+import turtle
+import time
+
+input ('엔터를 치면 거북이를 소개합니다.^^')
+
+turtle.shape('turtle')
+
+print('앞으로 전진합니다.')
+time.sleep(1)
+turtle.forward(100)
+
+print('한번더 앞으로 전진합니다.')
+time.sleep(1)
+turtle.forward(100)
+
+print('왼쪽으로 전진합니다.')
+time.sleep(1)
+turtle.left(90)
+turtle.forward(100)
+
+print('오른쪽으로 전진합니다.')
+time.sleep(1)
+turtle.right(90)
+turtle.forward(100)
+
+turtle.done()
+
+# 거북이로 다각형 그리기
+
+# turtle.pensize(10)
+#
+# # 사각형 그리기
+# input('엔터를 치면 사각형을 그립니다.')
+#
+# turtle.color("red")
+# turtle.forward(200)
+#
+# turtle.left(90)
+# turtle.forward(200)
+#
+# turtle.left(90)
+# turtle.forward(200)
+#
+# turtle.left(90)
+# turtle.forward(200)
+#
+# turtle.done()
+
+
+# 삼각형 그리기
+# input('엔터를 치면 초록색 삼각형을 그립니다.')
+#
+# turtle.color("green")
+#
+# turtle.right(30)
+# turtle.forward(300)
+#
+# turtle.left(120)
+# turtle.forward(300)
+#
+# turtle.left(120)
+# turtle.forward(300)
+#
+# turtle.done()
